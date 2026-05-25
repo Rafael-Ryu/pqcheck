df08dc154dfdec0b406929846d1c97f3c5a1079e
Rafael Ryu <rafael.nakahara@sou.inteli.edu.br>
2026-05-25 16:18:35 -0300

    test(detectors): add Go detector fixtures and integration tests

---
+++ tests/fixtures/go/edge_syntax_error.go
@@ -0,0 +1,5 @@
+package main
+
+func main( {
+	@@@ this is not valid go %%%
+}
---
+++ tests/fixtures/go/known_good.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/mlkem"
+	"crypto/sha256"
+	"golang.org/x/crypto/chacha20poly1305"
+)
+
+func main() {
+	aes.NewCipher(key)
+	sha256.New()
+	mlkem.GenerateKey768()
+	chacha20poly1305.New(key)
+}
